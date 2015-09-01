c04e6c59a4707ce30cd77232c8c166eee2fe20b0
dev-bulkhouse <developer@bulkhouse.com>
2015-09-01 17:13:30 +0530

    ads

--- application/views/forms/document_form.php
+++ application/views/forms/document_form.php
@@ -1,27 +1,27 @@
-<div class="md-modal md-effect-10" id="<?= $id_modal; ?>">
+<div class="md-modal md-effect-10" id="<?php echo  $id_modal; ?>">
     <div class="md-content">
 
         <?php if ($doc_title == "Partnership Deed Details" || $doc_title == "Shop establishment/Trade Licence" || $doc_title == "Certificate of Incorporation Details" || $doc_title == "Memorandum Of Association" || $doc_title == "Articles Of Association" || $doc_title == "Resident Address Proof" || $doc_title == "Buiseness Address Proof") { ?>
 
-        <h3 style="font-size: 2em"><?= $doc_title; ?>  <a style="cursor: default; color: white" class="md-close" aria-label="Close">&#215;</a></h3>
+        <h3 style="font-size: 2em"><?php echo  $doc_title; ?>  <a style="cursor: default; color: white" class="md-close" aria-label="Close">&#215;</a></h3>
         <div>
-            <form id="<?=$doc_id;?>" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="email" id="email" value="<?= $email; ?>">
+            <form id="<?php echo $doc_id;?>" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="email" id="email" value="<?php echo  $email; ?>">
                 <div class="form-group required">
-                    <label for="<?=$doc_id;?>"><?=$doc_placeholder;?>:</label>
-                    <input type="hidden" class="form-control" name="<?=$doc_id;?>" id="<?=$doc_id;?>">
+                    <label for="<?php echo $doc_id;?>"><?php echo $doc_placeholder;?>:</label>
+                    <input type="hidden" class="form-control" name="<?php echo $doc_id;?>" id="<?php echo $doc_id;?>">
 
                 </div>
-                <input type="hidden" name="email" id="email" value="<?= $email; ?>">
+                <input type="hidden" name="email" id="email" value="<?php echo  $email; ?>">
                 <div class="form-group m-t-lg">
 
                     <div class="col-lg-12 media m-t-none">
 
                         <div class="media-body">
-                            <input type="file" name="<?= $doc_file; ?>" title="Browse" class="btn btn-sm btn-info m-b-sm">
+                            <input type="file" name="<?php echo  $doc_file; ?>" title="Browse" class="btn btn-sm btn-info m-b-sm">
 
                         </div>
-                        <span><?= $doc_note; ?></span>
+                        <span><?php echo  $doc_note; ?></span>
                     </div>
                     <hr>
                     <div class="col-lg-2">
@@ -29,8 +29,8 @@
 
                     </div>
                     <div class="col-lg-4">
-                        <div id="<?= $doc_spinner; ?>" style="display: none"><i class="fa fa-spinner fa-pulse"></i></div>
-                        <div id="<?= $doc_status; ?>"></div>
+                        <div id="<?php echo  $doc_spinner; ?>" style="display: none"><i class="fa fa-spinner fa-pulse"></i></div>
+                        <div id="<?php echo  $doc_status; ?>"></div>
 
                     </div>
                 </div>
@@ -42,25 +42,25 @@
 
       <?php  } else { ?>
 
-        <h3 style="font-size: 2em"><?= $doc_title; ?>  <a style="cursor: default; color: white" class="md-close" aria-label="Close">&#215;</a></h3>
+        <h3 style="font-size: 2em"><?php echo  $doc_title; ?>  <a style="cursor: default; color: white" class="md-close" aria-label="Close">&#215;</a></h3>
         <div>
-            <form id="<?=$doc_id;?>" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="email" id="email" value="<?= $email; ?>">
+            <form id="<?php echo $doc_id;?>" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="email" id="email" value="<?php echo  $email; ?>">
                 <div class="form-group required">
-                    <label for="<?=$doc_id;?>"><?=$doc_placeholder;?>:</label>
-                    <input type="text" <?=$doc_pat;?> class="form-control" name="<?=$doc_id;?>" id="<?=$doc_id;?>" placeholder="<?=$doc_placeholder;?>" required="required">
+                    <label for="<?php echo $doc_id;?>"><?php echo $doc_placeholder;?>:</label>
+                    <input type="text" <?php echo $doc_pat;?> class="form-control" name="<?php echo $doc_id;?>" id="<?php echo $doc_id;?>" placeholder="<?php echo $doc_placeholder;?>" required="required">
 
                 </div>
-                <input type="hidden" name="email" id="email" value="<?= $email; ?>">
+                <input type="hidden" name="email" id="email" value="<?php echo  $email; ?>">
                 <div class="form-group m-t-lg">
 
                     <div class="col-lg-12 media m-t-none">
 
                         <div class="media-body">
-                            <input type="file" name="<?= $doc_file; ?>" title="Browse" class="btn btn-sm btn-info m-b-sm">
+                            <input type="file" name="<?php echo  $doc_file; ?>" title="Browse" class="btn btn-sm btn-info m-b-sm">
 
                         </div>
-                        <span><?= $doc_note; ?></span>
+                        <span><?php echo  $doc_note; ?></span>
                     </div>
                     <hr>
                     <div class="col-lg-2">
@@ -68,8 +68,8 @@
 
                     </div>
                     <div class="col-lg-4">
-                        <div id="<?= $doc_spinner; ?>" style="display: none"><i class="fa fa-spinner fa-pulse"></i></div>
-                        <div id="<?= $doc_status; ?>"></div>
+                        <div id="<?php echo  $doc_spinner; ?>" style="display: none"><i class="fa fa-spinner fa-pulse"></i></div>
+                        <div id="<?php echo  $doc_status; ?>"></div>
 
                     </div>
                 </div>
