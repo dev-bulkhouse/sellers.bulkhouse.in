ae6c5d9b82550752b3a7ba39078209b2b0638f96
dev-bulkhouse <developer@bulkhouse.com>
2015-09-09 14:10:12 +0530

    done

--- application/views/admin/preview_details.php
+++ application/views/admin/preview_details.php
@@ -9,7 +9,7 @@
 
 
                                                     <th>Company Name</th>
-                                                   
+
                                                     <th>Date of Submission</th>
                                                     <th>Action</th>
 
@@ -20,7 +20,7 @@
 
 
                                                     <td><?php echo $company_name; ?></td>
-                                                    
+
                                                     <td><?php echo $document_date; ?></td>
                                                     <td><form style="float: left" method="post" action="/change/approve/<?php echo $compid .$button ?>"><button type="submit" class="btn btn-success active">Approve</button> </form><form style="float: right" method="post" action="/change/disapprove/<?php echo $compid . '/'.$button ?>"><button type="submit" class="btn btn-danger active">Disapprove</button></form></td>
 
@@ -39,17 +39,17 @@
                                 <div class="panel-body panel-body-pricing">
                                     <h3>
                                                     <th><?php echo $document_name ?></th>
-                                                    <?php }else{} ?><br/><small>  
+                                                    <?php }else{} ?><br/><small>
                                                       <?php if(isset($document)){ ?>
                                                     <td><?php echo $document; ?></td>
                                                       </small></h3>
-                                 
+
                                 </div>
-                              
+
                             </div>
 
                         </div>
-                                <?php }else{} ?>  
+                                <?php }else{} ?>
                                          <?php if(isset($address1)){ ?>
                                          <div class="col-md-6">
 
@@ -57,18 +57,18 @@
                                 <div class="panel-body panel-body-pricing">
                                     <h3>
                                                     <th><?php echo $address ?></th>
-                                                    <?php }else{} ?><br/><small>  
+                                                    <?php }else{} ?><br/><small>
                                                       <?php if(isset($address1)){ ?>
                                                     <td><?php echo $address1; ?><br><?php echo $address2; ?></td>
                                                       </small></h3>
-                                 
+
                                 </div>
-                              
+
                             </div>
 
                         </div>
-                                <?php }else{} ?>    
-                                       
+                                <?php }else{} ?>
+
                                         <!--                                                                                            <div class="modal-body">
                                                                                                                                         <img src="<?php echo site_url(); ?>files/<?php echo $cert->compid; ?>/certification_of_incorporation.<?php echo $cert->cert_of_incorp_type; ?>" class="img-responsive">
                                                                                                                                     </div>-->
@@ -76,12 +76,14 @@
 $pan_prop_type = $type;
 
 $filejpg = "http://sellers.bulkhouse.files.s3.amazonaws.com/".$compid.$file_name.".jpg";
+$filejpg_a = "http://sellers.bulkhouse.files.s3.amazonaws.com/".$compid.$file_name.".JPG";
 $filepdf = "http://sellers.bulkhouse.files.s3.amazonaws.com/".$compid.$file_name.".pdf";
-if ($pan_prop_type == $filejpg){ ?>
+$filepdf_a = "http://sellers.bulkhouse.files.s3.amazonaws.com/".$compid.$file_name.".PDF";
+if ($pan_prop_type == $filejpg || $pan_prop_type == $filejpg_a){ ?>
               <div class="modal-body">
                            <img src="<?php echo $pan_prop_type; ?>" class="img-responsive">
                         </div>
-<?php }elseif ($pan_prop_type == $filepdf){ ?>
+<?php }elseif ($pan_prop_type == $filepdf || $pan_prop_type == $filepdf_a){ ?>
  <div class="modal-body">
      <iframe  width="100%" height="1000px" src="<?php echo $pan_prop_type; ?>" class="img-responsive" >
                         </div>
