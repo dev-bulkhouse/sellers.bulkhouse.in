f1d9ed8f5be418ffc33265e8e9f8bd45bb93aecb
dev-bulkhouse <developer@bulkhouse.com>
2015-09-25 17:58:56 +0530

    done

--- application/views/admin/employee.php
+++ application/views/admin/employee.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="utf-8">
         <title>Add Employee | Sellers.Bulkhouse</title>
         <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="stylesheet" href="/css/app.v1.css">
+         <link rel="stylesheet" type="text/css" id="theme" href="/css/theme-default.css"/>
         <link rel="stylesheet" href="/css/font.css" cache="false">
         <!--[if lt IE 9]>
         <script src="js/ie/respond.min.js" cache="false"></script>
@@ -25,8 +27,8 @@
         </style>
     </head>
 
-    <body style="background-color: white">
-
+    <body  class="page-container-boxed">
+        <div class="page-content-wrap">
         <section>
             <div class="row m-n animated fadeInDown">
                 <div class="col-md-12 col-lg-4 col-md-offset-4  m-t-lg">
@@ -56,12 +58,13 @@
             <div class="row m-n animated fadeInDown">
                 <div class="col-lg-8 centered col-lg-offset-2">
                     <section class="panel">
-                        <header class="panel-heading bg bg-primary text-center"> Edit Agent</header>
+                        <header class="panel-heading bg bg-primary text-center"> Edit Lead</header>
                         <div class="panel-body">
                             <table class="table datatable">
                                 <thead>
-                                <th>ID</th>
-                                <th>NAME</th>
+                                <th>Vendor Name</th>
+                                <th>Vendor e-mail</th>
+                                  <th>Agent ID</th>
 
                                 <th>ACTION</th>
 
@@ -69,16 +72,17 @@
                                 <tbody>
                                     <?php
                                     $result = $this->register_model->viewleads();
-                                    foreach ($result as $row) {
+                                    foreach ($result as $row) { ?>
 
-                                        echo "<tr>
-								<td> $row->vendor_email </td>
-								<td> $row->agent_id </td>
+                                      <tr>
+								<td><?php echo $row->vendor_name ?></td>
+                                                                <td> <?php echo $row->vendor_email ?> </td>
+								<td><?php echo $row->agent_id ?> </td>
 
-                                <td><a href='" . site_url('admin/edit_lead/' . $row->id) . "'>Edit</a> | <a href='" . site_url('admin/delete_lead/' . $row->id) . "'>Delete</a></td>
-								</tr>";
-                                    }
-                                    ?>
+                                <td><a class="btn btn-info active" href="<?php echo site_url()?>admin/edit_lead/<?php echo $row->id; ?>">Edit</a> | <a class="btn btn-danger active" href=<?php echo site_url()?>admin/delete_lead/<?php echo $row->id; ?>">Delete</a></td>
+								</tr>
+                                    <?php } ?>
+
                                 </tbody>
                             </table>
                         </div>
@@ -88,7 +92,32 @@
                 </div>
             </div>
         </section>
+            </div>
+  <audio id="audio-alert" src="/audio/alert.mp3" preload="auto"></audio>
+        <audio id="audio-fail" src="/audio/fail.mp3" preload="auto"></audio>
+        <!-- END PRELOADS -->
 
+        <!-- START SCRIPTS -->
+        <!-- START PLUGINS -->
+        <script type="text/javascript" src="/js/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/plugins/jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="/js/plugins/bootstrap/bootstrap.min.js"></script>
+        <!-- END PLUGINS -->
+
+        <!-- THIS PAGE PLUGINS -->
+        <script type='text/javascript' src='/js/plugins/icheck/icheck.min.js'></script>
+        <script type="text/javascript" src="/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
+
+        <script type="text/javascript" src="/js/plugins/datatables/jquery.dataTables.min.js"></script>
+        <!-- END PAGE PLUGINS -->
+
+        <!-- START TEMPLATE -->
+        <script type="text/javascript" src="js/settings.js"></script>
+
+        <script type="text/javascript" src="/js/plugins.js"></script>
+        <script type="text/javascript" src="/js/actions.js"></script>
+        <!-- END TEMPLATE -->
+        <!-- END SCRIPTS -->
 
 
 
