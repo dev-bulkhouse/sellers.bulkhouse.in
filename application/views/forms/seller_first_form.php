46665186e34c139e96553c97b21c9c8cb51418b9
dev-bulkhouse <developer@bulkhouse.com>
2015-10-07 15:25:37 +0530

    done

--- application/views/forms/seller_first_form.php
+++
@@ -1,2 +0,0 @@
- <?php $this->load->view('alert/success-message'); ?>
-<div class="small-12 columns  zeropadding" style="min-height: 800px;background-image: url('img/main-banner.jpg'); background-size: cover">
