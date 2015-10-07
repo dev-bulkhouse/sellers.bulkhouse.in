be0911fcd7463a9ec32c5842bd34895612065292
dev-bulkhouse <developer@bulkhouse.com>
2015-10-07 13:42:06 +0530

    dels

--- controllersDocs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Admin"],["c","Admin2"],["c","Admin_main"],["c","Amazon"],["c","Change"],["c","Confirm_bank"],["c","Create_table"],["c","Create_tables"],["c","Email"],["c","Home"],["c","Intro"],["c","Login"],["c","Mail"],["c","Main"],["c","Manage"],["c","Old"],["c","Register"],["c","Subscribe"],["c","Test"],["c","Upload"],["c","Upload_new"],["c","User"],["c","Welcome"],["c","What"]];
--- modelsDocs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","File_Model"],["f","insert_images()"],["c","Login_model"],["c","Mail_model"],["c","Register_model"],["c","Subscribe_model"],["c","Tabs"],["c","Users"],["c","Vendor_model"],["c","Vendor_update"]];
