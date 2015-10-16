74a10fc53d69834490e35d8f72462d46562bc5ee
dev-bulkhouse <developer@bulkhouse.com>
2015-10-17 03:19:11 +0530

    done

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -27,7 +27,7 @@ class Form extends CI_Controller {
             if ($result == 1) {
                 redirect('http://www.bulkhouse.com', 'location');
             }  else {
-                
+                redirect('http://www.bulkhouse.in', 'location');
             }
 
         }
