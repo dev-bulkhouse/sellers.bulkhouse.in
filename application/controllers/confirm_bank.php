745ac8d58bfa0e2e6922cf4c0f3d90f620d2e999
dev-bulkhouse <developer@bulkhouse.com>
2015-11-12 11:14:18 +0530

    done

--- application/controllers/confirm_bank.php
+++ application/controllers/confirm_bank.php
@@ -1,24 +1,25 @@
 <?php
-
+​
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
+​
 class Confirm_bank extends CI_Controller {
       function __construct() {
         parent::__construct();
-
+        $this->load->model('register_model');
+​
     }
-
+​
     public function amount($cid) {
-
+​
         $confirm_amount = $this->input->post('confirm_amount');
         $amount = $this->amt_chk($cid);
-
+​
         if ($confirm_amount == $amount) {
-
+​
             $this->bank_approved($cid);
             redirect(base_url().'main/','location');
-
+​
         }elseif($confirm_amount != $amount){
             $atmpt = $this->atmpt_chk($cid);
             if ($atmpt >= 3) {
@@ -28,7 +29,7 @@ class Confirm_bank extends CI_Controller {
             $this->db->where(array('bank_details.compid' => $cid));
             $this->db->update('bank_details',$data);
             redirect(base_url().'main/','location');
-
+​
             }else{
                 $atmpt = $this->atmpt_chk($cid);
             $data= array(
@@ -36,14 +37,28 @@ class Confirm_bank extends CI_Controller {
             );
             $this->db->where(array('bank_details.compid' => $cid));
             $this->db->update('bank_details',$data);
-
+​
            redirect(base_url().'main/','location');
+​
 
-           
             }}}
+    public function reset_attempts($email) {
 
+        $cid = $this->register_model->get_compid($email);
+​
+       $data = array(
+
+           'status' => 2,
+           'attempts' => 0
+
+        );
+        $this->db->where('compid', $cid);
+        $this->db->update('bank_details', $data);
+//        return 1;
+
+    }
     public function amt_chk($cid) {
-
+​
         $this->db->select('*');
         $this->db->from('bank_details');
         $this->db->where(array('bank_details.status' => 2));
@@ -51,10 +66,10 @@ class Confirm_bank extends CI_Controller {
         $query = $this->db->get();
         $bnk = $query->result();
         return $bnk[0]->amount;
-
+​
     }
     public function atmpt_chk($cid) {
-
+​
         $this->db->select('*');
         $this->db->from('bank_details');
         $this->db->where(array('bank_details.status' => 2));
@@ -62,18 +77,18 @@ class Confirm_bank extends CI_Controller {
         $query = $this->db->get();
         $bnk = $query->result();
         return $bnk[0]->attempts;
-
+​
     }
-
+​
     public function bank_approved($cid) {
          $data = array(
                'status' => 4,
          );
-
+​
 $this->db->where('compid', $cid);
 $this->db->update('bank_details', $data);
-
-
+​
+​
     }
-
+​
 }
