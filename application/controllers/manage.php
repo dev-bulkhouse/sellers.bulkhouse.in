77a2b18369cf0655cb4c1b63a7adac4d5109f011
dev-bulkhouse <developer@bulkhouse.com>
2015-11-18 23:13:40 +0530

    done

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -154,4 +154,228 @@ $this->load->helper('url');
             query_to_csv($quer,TRUE,'document_details'.'.csv');
  }
 
+ public function req_doc()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM doc_req');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('doc_req');
+
+            query_to_csv($quer,TRUE,'doc_req'.'.csv');
+ }
+
+ public function mag_cust()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM mag_cust');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('mag_cust');
+
+            query_to_csv($quer,TRUE,'mag_cust'.'.csv');
+ }
+
+ public function sec_vendor_authinfo()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM sec_vendor_authinfo');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('sec_vendor_authinfo');
+
+            query_to_csv($quer,TRUE,'sec_vendor_authinfo'.'.csv');
+ }
+
+ public function leads()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM leads');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('leads');
+
+            query_to_csv($quer,TRUE,'leads'.'.csv');
+ }
+
+ public function employee()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM employee');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('employee');
+
+            query_to_csv($quer,TRUE,'employee'.'.csv');
+ }
+
+ public function vendor_mobile()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM vendor_mobile');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('vendor_mobile');
+
+            query_to_csv($quer,TRUE,'vendor_mobile'.'.csv');
+ }
+
+ public function update_seller()
+{
+$this->load->helper('url');
+                $this->load->helper('csv');
+
+            $query = $this->db->query('SELECT * FROM update_seller');
+            $num = $query->num_fields();
+            $var =array();
+            $i=1;
+            $fname="";
+            while($i <= $num){
+                $test = $i;
+                $value = $this->input->post($test);
+
+                if($value != ''){
+                        $fname= $fname." ".$value;
+                        array_push($var, $value);
+
+                    }
+                 $i++;
+            }
+
+            $fname = trim($fname);
+
+            $fname=str_replace(' ', ',', $fname);
+
+            $this->db->select($fname);
+            $quer = $this->db->get('update_seller');
+
+            query_to_csv($quer,TRUE,'update_seller'.'.csv');
+ }
+
 }
